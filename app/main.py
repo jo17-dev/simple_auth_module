a5c0a5e624e97c82d71c4081f4aba2d4dfadfbdf
jo17-dev <procompter1@gmail.com>
2026-01-10 23:26:36 -0500

    renamed app dockerfile and updated .env.example

--- app/main.py
+++ app/main.py
@@ -3,10 +3,12 @@ from data.database import get_db
 from app.interface.routers.auth import router as auth_router
 from app.interface.routers.user import router as user_router
 from app.config.logger import logger
+from app.config.env import settings
 
 app = FastAPI(
     title="S.A.M - simple auth module", 
-    description="sam is a fastapi module who manages authentification via tokens and users via thiers email, password and roles "
+    description="sam is a fastapi module who manages authentification via tokens and users via thiers email, password and roles ",
+    version=settings.API_VERSION
     )
 app_router = APIRouter()
 app_router.include_router(auth_router, prefix="/auth", tags=["Token management"])
